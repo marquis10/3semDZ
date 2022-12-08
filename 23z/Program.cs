9cf1694d9f6930d531417f02fc81128b8b9b167a
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-08 13:24:44 +0500

    23z_last

--- 23z/Program.cs
+++ 23z/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Программа выполняет возведение чисел в куб от 1 до n");
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine()), i;
+for (i = 1; i <= n; i++)
+Console.WriteLine($"Число {i} в кубе = {Math.Pow(i, 3)}");
