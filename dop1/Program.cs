b2e9807f4667e8efbcbf8c129d157a1c1c5ad856
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-08 20:21:46 +0500

    19z_нумерация строк

--- dop1/Program.cs
+++ dop1/Program.cs
@@ -1,10 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*
-string? s = Console.ReadLine();
-Console.WriteLine(Convert.ToInt32(s[0].ToString())*6);
-*/
-
-Console.Clear();
+﻿Console.Clear();
 Console.WriteLine("Введите пятизначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 while (n > 99999 || n < 10000)
@@ -12,34 +6,8 @@ while (n > 99999 || n < 10000)
     Console.WriteLine("Введено неверное число, введите заного: ");
     n = Convert.ToInt32(Console.ReadLine());
 }
-n = Convert.ToString(n);
-if (n[0] == n[4] && n[1] == n[3])
+string m = Convert.ToString(n);
+if (m[0] == m[4] && m[1] == m[3])
     Console.WriteLine("Число является палиндромом");
 else
     Console.WriteLine("Число не является палиндромом");
-
-/*
-while (n > 99999 || n < 10000)
-{
-    Console.WriteLine("Введено неверное число, введите заного: ");
-    n = Convert.ToInt32(Console.ReadLine());
-}
-
-*/
-/*
-n4 = n % 10;
-n = n / 10;
-n3 = n % 10;
-n = n / 10;
-n2 = n % 10;
-n = n / 10;
-n1 = n % 10;
-n = n / 10;
-n0 = n;
-if (n0 == n4 && n1 == n3)
-{
-    Console.WriteLine("Число является палиндромом");
-}
-else
-    Console.WriteLine("Число не является палиндромом");
-    */
